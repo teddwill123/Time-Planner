ce1e6360d70a6244e5cfadd346fccd4bb6a9b5cc
teddwill123 <teddywilliejr@gmail.com>
2024-01-27 12:50:07 +0530

    modified the js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,159 +6,92 @@ $( document ).ready(function() {
     // Setting Global Variables
     var today = dayjs();                                                                            
     var currentHour = dayjs();                                                                      
-    var workingDayHours = [                                                                        
-        {
-            hourInt: 0, // [0]
-            hour: "12AM", // [1]
-            task: "", // [2]
-        },
-        {
-            hourInt: 1, // [0]
-            hour: "1AM", // [1]
-            task: "", // [2]
-        },
-        {
-            hourInt: 2, // [0]
-            hour: "2AM", // [1]
-            task: "", // [2]
-        },
-        {
-            hourInt: 3, // [0]
-            hour: "3AM", // [1]
-            task: "", // [2]
-        },
-        {
-            hourInt: 4, // [0]
-            hour: "4AM", // [1]
-            task: "", // [2]
-        },
-        {
-            hourInt: 5, // [0]
-            hour: "5AM", // [1]
-            task: "", // [2]
-        },
-        {
-            hourInt: 6, // [0]
-            hour: "6AM", // [1]
-            task: "", // [2]
-        },
-        {
-            hourInt: 7, // [0]
-            hour: "7AM", // [1]
-            task: "", // [2]
-        },
-        {
-            hourInt: 8, // [0]
-            hour: "8AM", // [1]
-            task: "", // [2]
-        },
+    var workingDayHours = [                                                                         
         {
             hourInt: 9, // [0]
             hour: "9AM", // [1]
-            task: "", // [2]
         },
         {
             hourInt: 10, // [0]
             hour: "10AM", // [1]
-            task: "", // [2]
         },
         {
             hourInt: 11, // [0]
             hour: "11AM", // [1]
-            task: "", // [2]
         },
         {
             hourInt: 12, // [0]
             hour: "12PM", // [1]
-            task: "", // [2]
         },
         {
             hourInt: 13, // [0]
             hour: "1PM", // [1]
-            task: "", // [2]
         },
         {
             hourInt: 14, // [0]
             hour: "2PM", // [1]
-            task: "", // [2]
         },
         {
             hourInt: 15, // [0]
             hour: "3PM", // [1]
-            task: "", // [2]
         },
         {
             hourInt: 16, // [0]
             hour: "4PM", // [1]
-            task: "", // [2]
         },
         {
             hourInt: 17, // [0]
             hour: "5PM", // [1]
-            task: "", // [2]
-        },
-        {
-            hourInt: 18, // [0]
-            hour: "6PM", // [1]
-            task: "", // [2]
-        },
-        {
-            hourInt: 19, // [0]
-            hour: "7PM", // [1]
-            task: "", // [2]
-        },
-        {
-            hourInt: 20, // [0]
-            hour: "8PM", // [1]
-            task: "", // [2]
-        },
-        {
-            hourInt: 21, // [0]
-            hour: "9PM", // [1]
-            task: "", // [2]
-        },
-        {
-            hourInt: 22, // [0]
-            hour: "10PM", // [1]
-            task: "", // [2]
-        },
-        {
-            hourInt: 23, // [0]
-            hour: "11PM", // [1]
-            task: "", // [2]
         },
     ];
-    var hourDisplayText = $("<p>");
-    hourDisplayText.addClass("currentHour");
-    $("header").append(hourDisplayText);
+
+    // Declaring a variable which will create a <p> element.
+    var hourDisplayPara = $("<p>");                                                                 
+    hourDisplayPara.addClass("currentHour");
+    // Appending the <p> element to the header.                                                   
+    $("header").append(hourDisplayPara);                                                            
 
     // Displaying Date
-    var hourDisplayText = $("<p>");
+    var hourDisplayPara = $("<p>");                                                                 
     $("#currentDay").text(today.format("[Today is:] dddd[,] MMMM Do"));                             
-    $(".currentHour").text(currentHour.format("[Current hour:] HH[:00]"));                           
+    $(".currentHour").text(currentHour.format("[Current hour:] HH[:00]"));                          
     
     // Timeblocks
     function dailyTimeblocks (hours) {                                                              
+        var tasksList = JSON.parse(localStorage.getItem("userTasks"));                              
+    // Checking if the taskList is falsy or not defined.
+        if (!tasksList) {                                                                           
+            tasksList = [];                                                                         
+            for (var i = 9; i <= 18; i++) {                                                         
+                tasksList.push({taskTime: i, taskText: ""})                                         
+            };
+    // Stores the tasksList in localstorage as a string.
+            localStorage.setItem("userTasks", JSON.stringify(tasksList));                           
+        };
+    // Iterate through the array.
         for (var i = 0; i < hours.length; i++) {                                                    
 
             var timeblockRow = $("<div>");                                                          
             timeblockRow.addClass("row");                                                           
-
+    // Setting a variable to create a div.
             var hourSlot = $("<div>");                                                              
             hourSlot.addClass("hour col-1");                                                        
             hourSlot.text(hours[i].hour);                                                           
             timeblockRow.append(hourSlot);                                                          
-
+    // Setting a variable to create a textarea.
+    // Set text content of userTask textarea to the taskText from the corresponding index in tasksList
             var userTask = $("<textarea>");                                                         
             userTask.addClass("description col");                                                  
             userTask.attr("data-index", hours[i].hourInt);                                          
-            timeblockRow.append(userTask);                                                          
+            userTask.text(tasksList[i].taskText);                                                   
+            timeblockRow.append(userTask);                                                         
             
-            var saveTask = $("<button>");                                                           
-            saveTask.addClass("saveBtn col-1");                                                     
+            var saveTask = $("<button><i>");                                                        
+            saveTask.addClass("saveBtn col-1 fas fa-save fa-2x");                                   
+            saveTask.css("color:#ffffff");// Adding the color white to the icon.                                                   
             timeblockRow.append(saveTask);                                                          
 
-            $(".container").append(timeblockRow);                                                   
+            $(".container").append(timeblockRow);// Appending each timeblockRow div to the timeblocks container.
         };
     }
     dailyTimeblocks(workingDayHours);                                                               
@@ -167,50 +100,74 @@ $( document ).ready(function() {
         var currentHourNumber = currentHour.format("HH");                                           
         var currentHourInt = parseInt(currentHourNumber);                                           
 
-        for (var i = 0; i < workingDayHours.length; i++) {                                        
+        for (var i = 0; i < workingDayHours.length; i++) {                                         
             if (workingDayHours[i].hourInt === currentHourInt) {                                    
                 var matchingUserTask = $("[data-index='" + workingDayHours[i].hourInt + "']");      
                 matchingUserTask.addClass("present");                                               
             };
         };
     }
-    presentHour();                                                                                  
-
+    presentHour(); // Calling the presentHour function.
+    
+    //Function to change the class of a timeblockRow's textarea based on the hour being lower than the current time.
     function pastHour () {                                                                          
         var currentHourNumber = currentHour.format("HH");                                           
-        var currentHourInt = parseInt(currentHourNumber);                                         
-
+        var currentHourInt = parseInt(currentHourNumber);                                           
+    // Iterate through the array.
         for (var i = 0; i < workingDayHours.length; i++) {                                          
             if (workingDayHours[i].hourInt < currentHourInt) {                                      
                 var matchingUserTask = $("[data-index='" + workingDayHours[i].hourInt + "']");      
-                matchingUserTask.addClass("past");                                               
+                matchingUserTask.addClass("past");                                                  
             };
         };
     }
-    pastHour();                                                                                     
+    pastHour(); // Calling the pastHour function.
 
-    function futureHour () {                                                                       
-        var currentHourNumber = currentHour.format("HH");                                        
-        var currentHourInt = parseInt(currentHourNumber);                                           
-
+    // Function to change the class of a timeblockRow's textarea based on the hour being greater than the current time.
+    function futureHour () {                                                                        
+        var currentHourNumber = currentHour.format("HH");                                           
+        var currentHourInt = parseInt(currentHourNumber);                                         
+    // Iterate through the array.
         for (var i = 0; i < workingDayHours.length; i++) {                                          
             if (workingDayHours[i].hourInt > currentHourInt) {                                      
                 var matchingUserTask = $("[data-index='" + workingDayHours[i].hourInt + "']");      
-                matchingUserTask.addClass("future");                                               
+                matchingUserTask.addClass("future");// Add the "future" class to the matching textarea.
             };
         };
     }
-    futureHour();                                                                                   
+    futureHour();// Calling the futureHour function.
 
-    function saveTaskEntry() {                                                                   
+    // Function to save the users task to local storage.
+    function saveTaskEntry() {                                                                      
         var clickedButton = $(this);                                                                
-        var correspondingTextarea = clickedButton.closest(".row").find("textarea");               
+        var correspondingTextarea = clickedButton.closest(".row").find("textarea");                 
+        var existingUserTasks = JSON.parse(localStorage.getItem("userTasks"));                      
+        var hourIntIndex = correspondingTextarea.data("index");                                     
 
         if (correspondingTextarea.val().trim() !== "") {                                            
-            console.log(correspondingTextarea.val());
-        } else {                                                                                    
-            console.log("Textarea is empty!");
-        }
+            var taskText = correspondingTextarea.val();                                             
+            var taskTime = hourIntIndex;                                                            
+            var taskListItems = {taskTime, taskText};                                               
+        } else {
+            // Alert to advise the user they must enter something in the text area to save.                                                                                    
+            alert("Please enter a task and try again!");                        
+            return;
+        };
+
+        if (!existingUserTasks) {                                                                   
+            existingUserTasks = [];                                                                 
+        };
+
+        for (var i = 0; i < existingUserTasks.length; i++) {                                        
+            if (existingUserTasks[i].taskTime === hourIntIndex) {                                   
+                existingUserTasks.splice(i, 1);                                                                               
+            };
+        };
+
+        // Creates newTasks array by spreading the elements of existingUserTasks and adding taskListItems to the end.
+        var newTasks = [...existingUserTasks, taskListItems];                                       
+        newTasks.sort((a, b) => a.taskTime-b.taskTime);                                             
+        localStorage.setItem("userTasks", JSON.stringify(newTasks));                                
     }
-    $(".saveBtn").click(saveTaskEntry);                                                          
+    $(".saveBtn").click(saveTaskEntry);// Calling the saveTaskEntry function when a save button is clicked.
 });
